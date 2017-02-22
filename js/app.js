d56cd5ea7f1fe1ac258b5792935a5c4bcf4e10fd
kleito870827 <kleito870827@gmail.com>
2017-02-21 19:47:06 -0500

    portfolio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,70 @@
 
-$("#link-tab-skill").click( function(){
+$("#link-tab-skill, #link-tab-worksExaples, #link-tab-aboutMe").click( function(){
   $("#img-start").css("background-image", "none");
   $(".progress").css("display", "block");
 });
+
+var cont = 0;
+
+$("#nextRight").on("click", function(){
+  jQuery.ajax("works.json",{
+    dataType: "json",
+    method: "GET",
+    success: function(resp){
+      if(cont < resp.length){
+          $(".nameWork").html(resp[cont].name);
+          $(".imgWork").attr("src", resp[cont].imgUrl);
+          $(".pWork").html(resp[cont].description);
+          cont++;
+    }else{
+      cont = 0;
+      $(".nameWork").html(resp[cont].name);
+      $(".imgWork").attr("src", resp[cont].imgUrl);
+      $(".pWork").html(resp[cont].description);
+      cont++;
+    }
+
+    },
+    error: function(xhr) {
+      console.log(xhr);
+    }
+  });
+})
+
+$("#nextLeft").on("click", function(){
+  jQuery.ajax("works.json",{
+    dataType: "json",
+    method: "GET",
+    success: function(resp){
+      if(cont >= 0){
+        if(cont == 0){
+          $(".nameWork").html(resp[cont].name);
+          $(".imgWork").attr("src", resp[cont].imgUrl);
+          $(".pWork").html(resp[cont].description);
+          cont = resp.length - 1;
+        }else if(cont == resp.length){
+          cont--;
+          $(".nameWork").html(resp[cont].name);
+          $(".imgWork").attr("src", resp[cont].imgUrl);
+          $(".pWork").html(resp[cont].description);
+          cont--;
+        }else{
+          $(".nameWork").html(resp[cont].name);
+          $(".imgWork").attr("src", resp[cont].imgUrl);
+          $(".pWork").html(resp[cont].description);
+          cont--;
+        }
+    }else{
+      cont = resp.length - 1;
+      $(".nameWork").html(resp[cont].name);
+      $(".imgWork").attr("src", resp[cont].imgUrl);
+      $(".pWork").html(resp[cont].description);
+      cont--;
+    }
+
+    },
+    error: function(xhr) {
+      console.log(xhr);
+    }
+  });
+})
